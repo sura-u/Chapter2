62026d38f8533227065f844a10aa4df6042600b4
TONG MAI <TONG MAI@OhBugTong>
2014-12-21 10:14:20 +0700

    commit2

--- src/main/java/com/tong/helloworld/HelloWorld.java
+++ src/main/java/com/tong/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ package com.tong.helloworld;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("Hello World2.");
         
     }
 }
